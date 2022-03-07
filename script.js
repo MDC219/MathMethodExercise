fa4bbf62c6f7f14eee02fba6ac6d6aa5e554a021
MDC 219 <mcouch219@gmail.com>
2022-03-06 19:46:10 -0500

    MissingQuotesinCode

---
+++ script.js
@@ -0,0 +1,49 @@
+let warmHugs = ("Hi, I am Olaf and I like warm hugs.");
+console.log(warmHugs);
+
+console.log(warmHugs);
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace ("like" , "love")); //1c
+
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace ("like" , "love"));//1c//
+//varuabke name = warmHugs.replace ("like" , "love"))//1c//
+//console log outputs the message to the web console.
+
+let beenImpaled = ("Oh. look at that. I\'ve been Impaled.");
+
+
+console.log(beenImpaled);
+console.log(beenImpaled.slice(18));//2b
+
+
+let Dotdotdot = ("Dotdotdot");
+
+console.log(Dotdotdot);
+
+let skullBones = (`I dont have a skull ${Dotdotdot} or bones.`);
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber *=3 ;//0 to 2.99999
+randomNumber = Math.floor(randomNumber);// removes decimal
+randomNumber++;
+
+console.log("Let it go!").toUpperCase().repeat(3).trim();
+
+let reindeers = "Reindeers are better than people.";
+
+
+console.log(reindeers.replace(/ /g, '_'));// or
+
+let squareroot = Math.sqrt(2);
+
+let newRandomNumber = Math.random();
+newRandomNumber *= 17;
+newRandomNumber+= 7;
+
+
